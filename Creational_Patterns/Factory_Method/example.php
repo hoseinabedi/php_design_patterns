da3cd9f0b3257343d6b6b6b18b81b3eed766ec2f
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-12-04 21:34:32 +0330

    Implementing Factory Method design pattern

--- Creational_Patterns/Factory_Method/example.php
+++ Creational_Patterns/Factory_Method/example.php
@@ -10,10 +10,86 @@ abstract class SocialNetworkPoster{
     }
 }
 
+class FacebookPoster extends SocialNetworkPoster{
+    private $username, $password;
+
+    public function __construct(string $username, string $password){
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function getSocialNetwork(): SocialNetworkConnector{
+        return new FacebookConnector($this->username, $this->password);
+    }
+}
+
+class TwitterPoster extends SocialNetworkPoster{
+    private $email, $password;
+
+    public function __construct(string $email, string $password){
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getSocialNetwork(): SocialNetworkConnector{
+        return new TwitterConnector($this->email, $this->password);
+    }
+}
+
 interface SocialNetworkConnector{
     public function login(): void;
-    public function createPost(): void;
+    public function createPost(string $content): void;
     public function logout(): void;
 }
+
+class FacebookConnector implements SocialNetworkConnector{
+    private $username, $password;
+
+    public function __construct(string $username, string $password){
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function login(): void{
+        echo "Call login Api to connect to facebook with username $this->username<br/>";
+    }
+    
+    public function createPost(string $content): void{
+        echo "Call createPost Api to create a post on facebook<br/>";
+    }
+
+    public function logout(): void{
+        echo "Call logout Api to disconnect from facebook with username $this->username<br/>";
+    }
+}
+
+class TwitterConnector implements SocialNetworkConnector{
+    private $email, $password;
+
+    public function __construct(string $email, string $password){
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function login(): void{
+        echo "Call login Api to connect to twitter with email $this->email</br>";
+    }
+
+    public function createPost(string $content): void{
+        echo "Call createPost Api to create a post on twitter</br>";
+    }
+
+    public function logout(): void{
+        echo "Call logout Api to disconnect from twitter with email $this->email</br>";
+    }
+}
+
+
+function clientCode(SocialNetworkPoster $create){
+    $create->post("Hello World!");
+}
+
+$client1 = clientCode(new FacebookPoster("h.abedi", "12345"));
+$client2 = clientCode(new TwitterPoster("hosein.abedi2000@gmail.com", "12345"));
 
 ?>
