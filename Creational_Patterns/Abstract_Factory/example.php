79a40eba958a88769e6db52d919fc0ccfa10b055
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-12-05 11:20:02 +0330

    Add class PHPTemplateRenderer

--- Creational_Patterns/Abstract_Factory/example.php
+++ Creational_Patterns/Abstract_Factory/example.php
@@ -81,6 +81,19 @@ class TwigTemplateRender implements TemplateRender{
     }
 }
 
+class PHPTemplateRenderer implements TemplateRender{
+    public function render(string $templateString, array $arguments = []): string{
+        extract($arguments);
+
+        ob_start();
+        eval(' ?>' . $templateString . '<?php ');
+        $result = ob_get_contents();
+        ob_end_clean();
+
+        return $result;
+    }
+}
+
 
 
 
